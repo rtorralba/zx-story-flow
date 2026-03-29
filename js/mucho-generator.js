4c21040dbd545135855ec36943b0e907ab5c3f2e
zx-moe <zxmoses@gmail.com>
2026-03-29 16:43:29 +0200

    fix: no empty space between screens. Use empty comment line

--- js/mucho-generator.js
+++ js/mucho-generator.js
@@ -146,7 +146,10 @@ export function generateMucho(nodes, globalConfig = null, startNodeId = null) {
             }
         });
 
-        muchoCode += "\n";
+        // Separate screens with a comment line, not an empty line.
+        // An empty line can create problems when processign subroutines
+        // as it can be confused as empty space inside the screen.
+        muchoCode += "#\n";
     });
 
     return muchoCode;
